56d6dab73e9ea90cd59296c5c07723b2aff1ad29
phongnguyengia <60820767+phongnguyengia@users.noreply.github.com>
2020-03-30 13:26:49 +0700

    Create PrintSheepPattern.java

---
+++ Lesson-1/PrintSheepPattern.java
@@ -0,0 +1,12 @@
+public class PrintSheepPattern {
+    public static void main(String[] args){
+        System.out.println("        '__'");
+        System.out.println("        (oo)");
+        System.out.println(" /=======//");
+        System.out.println("/|| @@ ||");
+        System.out.println(" ||----||");
+        System.out.println(" vv    vv    ");
+        System.out.println(" ''    ''   ");
+
+    }
+}
