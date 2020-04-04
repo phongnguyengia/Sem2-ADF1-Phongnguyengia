ddbc3e6bd96b3e3512bd316f38d5db5f5acb88a7
phongnguyengia <60820767+phongnguyengia@users.noreply.github.com>
2020-04-04 09:12:57 +0700

    Create EmployeeFullTime.java

---
+++ PRACTICE - BÀI THI THỰC HÀNH/EmployeeFullTime.java
@@ -0,0 +1,17 @@
+public class EmployeeFullTime extends SalaryPolicy {
+    private float rate;
+
+    public EmployeeFullTime() {
+        this.rate = 4;
+    }
+
+    public EmployeeFullTime(float baseSalary) {
+        super(baseSalary);
+        this.rate = 4;
+    }
+
+    @Override
+    float getSalary() {
+        return this.rate * super.getBaseSalary();
+    }
+}
